127183a100c8738bcafbc2d515dabdffeaf66ded
Brooke <brookewu2023@gmail.com>
2025-02-08 17:12:47 -0500

    Implement a Loading screen component in React

--- shelfsaver-frontend/src/App.jsx
+++ shelfsaver-frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Loading from './components/Loading';
 import './App.css'
 
 function App() {
@@ -6,12 +8,16 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // Code to run when App component is first mounted onto the DOM
   useEffect(() => {
     document.title = 'ShelfSaver';
 
     // Fetch data from API endpoint with a GET request mapped in the Spring Boot controller
     const fetchData = async() => {
       try {
+        // Set a timeout for 1000ms (1s) before fetching the data so that users get to see my beautiful loading screen <3
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
         const response = await fetch('http://localhost:8080/api/test');
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
@@ -28,7 +34,9 @@ function App() {
   }, []);
 
   if (loading) {
-    return <p>Loading data...</p>;
+    return (
+      <Loading/>
+    );
   }
 
   if (error) {
@@ -38,6 +46,7 @@ function App() {
   return (
     <>
       <p>Received &quot;{data}&quot; from Spring Boot backend</p>
+      <Button/>
     </>
   )
 }
---
+++ shelfsaver-frontend/src/components/Loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return(
+        <>
+            <p className="glowing-text">Loading your greatest grocery fantasy...</p>
+            <img src="src/assets/pixabay_fire.gif"></img>
+        </>
+    );
+}
