a84b28495b75b56c5d8663131fc60ddbcdbd4f71
avasaydam <ava.saydam@gmail.com>
2025-02-08 14:06:08 -0500

    changing info for new database

--- src/main/java/com/example/ShelfSaver/QuickStart.java
+++ src/main/java/com/example/ShelfSaver/QuickStart.java
@@ -11,9 +11,9 @@ public class QuickStart {
         // Replace the placeholder with your MongoDB deployment's connection string
         String uri = "mongodb+srv://avasaydam:iWYMGuCQOD6MnKjT@shelfsaver.fvczy.mongodb.net/?retryWrites=true&w=majority&appName=ShelfSaver";
         try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-            MongoCollection<Document> collection = database.getCollection("movies");
-            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            MongoDatabase database = mongoClient.getDatabase("shelfSaver");
+            MongoCollection<Document> collection = database.getCollection("items");
+            Document doc = collection.find(eq("name", "bananas")).first();
             if (doc != null) {
                 System.out.println(doc.toJson());
             } else {
