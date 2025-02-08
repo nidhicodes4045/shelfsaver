168f213503e30678e49e9894a9f510023625631f
Brooke <brookewu2023@gmail.com>
2025-02-08 11:59:16 -0500

    Add Spring Boot template

---
+++ src/main/java/com/example/ShelfSaver/ShelfSaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShelfSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShelfSaver/ShelfSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShelfSaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
