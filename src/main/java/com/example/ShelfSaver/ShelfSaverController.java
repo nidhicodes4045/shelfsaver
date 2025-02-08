1d62157e88d585321983754cd046c39a6b34f2af
Brooke <brookewu2023@gmail.com>
2025-02-08 12:29:22 -0500

    Add the web controller

---
+++ src/main/java/com/example/ShelfSaver/ShelfSaverController.java
@@ -0,0 +1,19 @@
+package com.example.ShelfSaver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// RestController flag indicates that this class is ready for use by Spring MVC to handle web requests
+// It combines @Controller and @ResponseBody, 2 annotations that result in web requests returning data rather than a view
+@RestController
+public class ShelfSaverController {
+    // Map '/' to the index() method
+    @GetMapping("/")
+    /**
+     * When invoked from a browser or by using curl on the command line, the method returns pure text
+     * @return String to be returned when the controller is invoked from browser or the command line
+     */
+    public String index() {
+        return "Hello, World!";
+    }
+}
