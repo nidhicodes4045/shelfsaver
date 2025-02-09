936aca0b9a846401ce6226774b2bbc6e1330eac3
Brooke <brookewu2023@gmail.com>
2025-02-09 08:50:51 -0500

    Add Back to Login button to Item page

--- shelfsaver-frontend/src/components/User.jsx
+++ shelfsaver-frontend/src/components/User.jsx
@@ -2,14 +2,16 @@ import { useParams } from "react-router";
 import { DisplayAllItems } from "./Item";
 
 export default function User() {
-    let { uid, role, location } = useParams();
+    let { uid } = useParams();
 
+    let role = "Supermarket (Seller)";
+    let location = "Raleigh, NC";
     return (
         <>
             <p>User UID: {uid}</p>
-            {role}
-            Location: {location}
-            Available Items: {DisplayAllItems}
+            <p>{role}</p>
+            <p>Location: {location}</p>
+            <p>Available Items: {DisplayAllItems}</p>
         </>
     );
 }
